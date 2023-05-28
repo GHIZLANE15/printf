274282490f2e09340a403cc14901a41e88829cbc
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-28 14:08:37 -0700

    printf

--- handle_p.c
+++ handle_p.c
@@ -1,36 +1,44 @@
 #include "main.h"
 
-/**
+ /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
+ * @ind: Pointer to the index of the current character in fmt.
  * @list: List of arguments to be printed.
- * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specifier
- * @size: Size specification
+ * @flags: Active flags.
+ * @width: Width specification.
+ * @precision: Precision specification.
+ * @size: Size specifier.
  *
- * Return: 1 or 2;
+ * Return: Number of characters printed.
  */
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	int unknow_len = 0;
-	char current_fmt = fmt[*ind];
 
-	for (int i = 0; fmt_type[i].fmt != '\0'; i++)
+	printed_chars = -1;
+	fmt_t fmt_types[] = {
+	{'c', print_char}, {'s', print_string}, {'%', print_percent},
+	{'i', print_int}, {'d', print_int}, {'b', print_binary},
+	{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+	{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+	{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+
+	for (i = 0; fmt_type[i].fmt != '\0'; i++)
 	{
-		if (current_fmt == fmt_types[i].fmt)
+		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 	}
 
-	if (current_fmt == '\0')
+	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, "%%", 1);
+		unknow_len += write(1, "%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)
@@ -45,5 +53,5 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
-	return (1);
+	return (printed_chars);
 }
