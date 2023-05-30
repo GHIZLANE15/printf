2175f6a55b736f1abd414363e06b35e1cf1bb311
Ghizlane <ghizlane.fe15@gmail.com>
2023-05-30 12:21:46 -0700

    printf

--- handle_p.c
+++ handle_p.c
@@ -11,7 +11,7 @@
  * @precision: Precision specification.
  * @size: Size specifier.
  *
- * Return: Number of characters printed.
+ * Return: Number  characters printed.
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		int flags, int width, int precision, int size)
