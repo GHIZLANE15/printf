652117301009c56864325954d003a8fc30714e3d
salwua <salouamhs@gmail.com>
2023-05-30 15:04:51 -0700

    salw

--- P_width.c
+++ P_width.c
@@ -12,13 +12,13 @@ int get_width(const char *format, int *i, va_list list)
 	int curr_i;
 	int width = 0;
 
-	while (curr_i = *i + 1; format[curr_i] != '\0')
+	for (curr_i = *i + 1; format[curr_i] != '\0';curr_i++)
 	{
 		if (is_digit(format[curr_i]))
 		{
 		width *= 10;
 		width += format[curr_i] - '0';
-			curr_i++;
+
 		}
 		else if (format[curr_i] == '*')
 		{
