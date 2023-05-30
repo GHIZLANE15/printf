306df6706a3d98006603e2da32b5b50eca64f6cc
salwua <salouamhs@gmail.com>
2023-05-30 14:10:58 -0700

    salw

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 	return (-1);
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -59,7 +59,7 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 	{
-		write(1, buffer, *buff_ind);
+		write(1, &buffer[0], *buff_ind);
 	}
 	*buff_ind = 0;
 }
