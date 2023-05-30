66aa3afbf4071bfbb1832402b33b0436ce33e2ae
salwua <salouamhs@gmail.com>
2023-05-30 08:19:00 -0700

    salw

--- w-handler.c
+++ w-handler.c
@@ -99,7 +99,7 @@ int write_num(int ind, char buffer[], int flags,
 
 	if (prec == 0 && ind == BUFSIZ - 2 && buffer[ind] == '0' && width == 0)
 		return (0);
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
+	if (prec == 0 && ind == BUFSIZ - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' ';
 
 	if (prec > 0 && prec < length)
@@ -125,7 +125,7 @@ int write_num(int ind, char buffer[], int flags,
 				buffer[--ind] = extra_c;
 			return (write(i, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
-		else if ((!(flags & F_MINUS) && padd == ' ')
+		else if (!(flags & F_MINUS) && padd == ' ')
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
