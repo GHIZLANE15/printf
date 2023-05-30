7a23cfcda76280bd173e6598c9b9a7e2ce30e47a
salwua <salouamhs@gmail.com>
2023-05-30 08:15:28 -0700

    salw

--- w-handler.c
+++ w-handler.c
@@ -22,7 +22,7 @@ int handle_write_char(char c, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
-	if (flages & F_ZERO)
+	if (flags & F_ZERO)
 		padd = '0';
 	buffer[i++] = c;
 	buffer[i] = '\0';
@@ -64,7 +64,7 @@ int write_number(int is_negative, int ind, char buffer[],
 {
 	char extra_ch = 0;
 	char padd = ' ';
-	int length = BUFF_size - ind - 1;
+	int length = BUFSIZ - ind - 1;
 
 	UNUSED(size);
 
@@ -97,7 +97,7 @@ int write_num(int ind, char buffer[], int flags,
 {
 	int i, padd_start = 1;
 
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
+	if (prec == 0 && ind == BUFSIZ - 2 && buffer[ind] == '0' && width == 0)
 		return (0);
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' ';
@@ -107,33 +107,35 @@ int write_num(int ind, char buffer[], int flags,
 		padd = ' ';
 
 	while (prec > length)
-
+	{
 		buffer[--ind] = '0';
 		length++;
-
+	}
 	if (extra_c != 0)
 		length++;
 	if (width > length)
-
+	{
 		for (i = 1; i < width - length + 1; i++)
 			buffer[i] = padd;
 		buffer[i] = '\0';
-
+	}
 		if ((flags & F_MINUS) && padd == ' ')
-
+		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(i, &buffer[ind], length) + write(1, &buffer[1], i - 1));
-
+		}
 		else if ((!(flags & F_MINUS) && padd == ' ')
-
+		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
-
+		}
 		else if (!(flags & F_MINUS) && padd == '0')
-		if (extra_c)
+
+			if (extra_c)
 			buffer[--pad_start] = extra_c;
+
 		int write1 = write(1, &buffer[padd_start], i - padd_start);
 		int write2 = write(1, &buffer[ind], length - (1 - padd_start));
 
